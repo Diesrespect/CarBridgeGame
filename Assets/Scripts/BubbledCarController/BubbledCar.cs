86f6f348b1980b26231a0c4ff5a2e4d9a952389a
Uladzislaw V <deffleppard@gmail.com>
2024-08-04 14:55:04 +0300

    My First Project On Git

---
+++ Assets/Scripts/BubbledCarController/BubbledCar.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbledCar : MonoBehaviour
+{
+    private float moveInput;
+    private float turnInput;
+
+    private bool isCarGrounded;
+
+    public LayerMask groundLayer;
+
+    private float normalDrag;
+    public float modifiedDrag;
+    
+    public float alighToGroundTime;
+    
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+    
+    public Rigidbody sphereRB;
+    public Rigidbody carRB;
+
+    private void Start()
+    {
+        sphereRB.transform.parent = null;
+        carRB.transform.parent = null;
+
+        normalDrag = sphereRB.drag;
+    }
+
+    private void Update()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+        
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+
+        transform.position = sphereRB.transform.position;
+
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0, newRotation, 0, Space.World);
+
+        RaycastHit hit;
+        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
+        
+        Quaternion toRotate = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotate, alighToGroundTime * Time.deltaTime);
+
+        sphereRB.drag = isCarGrounded ? normalDrag : modifiedDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isCarGrounded)
+        {
+            sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+        }
+        else
+        {
+            sphereRB.AddForce(transform.up * -40f);
+        }
+        carRB.MoveRotation(transform.rotation);
+      
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Tooltip("camera positions")] 
+    [SerializeField] Transform[] povs;
+    [Tooltip("camera following's speed")]
+    [SerializeField] private float speed;
+
+    private int index = 0;
+    private Vector3 target;
+
+    public Camera cam;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            index = 0;
+            cam.fieldOfView = 60f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            index = 1;
+            cam.fieldOfView = 40f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) index = 2;
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) index = 3;
+
+        target = povs[index].position;
+        
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.forward = povs[index].forward;
+    }
+
+    private void FixedUpdate()
+    {
+        /*transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.forward = povs[index].forward;*/
+    }
+}
